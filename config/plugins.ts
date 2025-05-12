8a3d7188a2ac6008702666939926b9e4b469da80
evalambert <145194730+evalambert@users.noreply.github.com>
2025-05-12 13:05:19 +0200

    first image

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,4 +1,4 @@
-/* export default ({ env }) => ({
+export default ({ env }) => ({
     
     'users-permissions': {
         config: {
@@ -22,35 +22,3 @@
     },
 });
 
- */
-export default ({ env }) => {
-  // Ajoute ce log pour déboguer la configuration
-  console.log('Cloudinary config:', {
-    cloud_name: env('CLOUDINARY_NAME'),
-    api_key: env('CLOUDINARY_KEY'),
-    api_secret: env('CLOUDINARY_SECRET') ? '***' : 'MISSING',
-  });
-
-  return {
-    'users-permissions': {
-      config: {
-        jwtSecret: env('JWT_SECRET'),
-      },
-    },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
-        },
-      },
-    },
-  };
-};
